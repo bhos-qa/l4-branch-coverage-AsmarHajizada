a6ddf4b65193110c8c16f4a8b3c0fc54ab12f33c
Asmar Hajizada <71691828+AsmarHajizada@users.noreply.github.com>
2022-10-01 21:54:49 +0400

    Update TestClassroom.java

--- src/test/java/org/example/TestClassroom.java
+++ src/test/java/org/example/TestClassroom.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
