aa18cd14c02af6a405ee1fb783b7f9c99861a958
Asmar Hajizada <71691828+AsmarHajizada@users.noreply.github.com>
2022-10-01 14:47:39 +0400

    Update TestClassroom.java

--- src/test/java/org/example/TestClassroom.java
+++ src/test/java/org/example/TestClassroom.java
@@ -15,31 +15,31 @@ public class TestClassroom {
 
     @Test
     @DisplayName("Name should be set")
-    void SetName() {
+    void testSetName() {
         assertEquals("Asmar", classroom.setName("Asmar"), "It should work for any name");
     }
 
     @Test
     @DisplayName("Age should be set")
-    void SetAge() {
+    void testSetAge() {
         assertEquals(15, classroom.setAge(15), "It should work for any age");
     }
 
     @Test
     @DisplayName("Address should be set")
-    void SetAddress() {
+    void testSetAddress() {
         assertEquals("Khatai dist. Ganja pr.", classroom.setAddress("Khatai dist. Ganja pr."), "It should work for any address");
     }
 
     @Test
     @DisplayName("Email should be set")
-    void SetEmail() {
+    void testSetEmail() {
         assertEquals("asmar@gmail.com", classroom.setEmail("asmar@gmail.com"), "It should work for any Email");
     }
 
     @Test
     @DisplayName("Classroom type should be set")
-    void SetClassroomType() {
+    void testSetClassroomType() {
         assertEquals(classroomType.MORNING, classroom.setClassoomType(classroomType.MORNING), "Clasroom type should be set for one of the two given types");
     }
 }
