3bba22145129d9391df68c16bc0af24744e4436d
Asmar Hajizada <71691828+AsmarHajizada@users.noreply.github.com>
2022-10-01 22:07:42 +0400

    Update Classroom.java

--- src/main/java/org/example/Classroom.java
+++ src/main/java/org/example/Classroom.java
@@ -4,11 +4,11 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Classroom {
-    String email;
+//    String email;
     int result;
-    String name;
-    int age;
-    String address;
+//    String name;
+//    int age;
+//    String address;
 
     public int add(int age1, int age2){
         result = age1 + age2;
@@ -25,31 +25,31 @@ public class Classroom {
         return result;
     }
 
-    public String setEmail(String newEmail){
-        this.email = newEmail;
-        return this.email;
-    }
+//    public String setEmail(String newEmail){
+//        this.email = newEmail;
+//        return this.email;
+//    }
 
     public float divide(int num1, int num2){
         result = num1/num2;
         return result;
     }
     
-    public String getName(){
-        return this.name;
-    }
+//    public String getName(){
+//        return this.name;
+//    }
 
-    public int getAge(){
-        return this.age;
-    }
+//    public int getAge(){
+//        return this.age;
+//    }
 
-    public String getAddress(){
-        return this.address;
-    }
+//    public String getAddress(){
+//        return this.address;
+//   }
 
-    public String getEmail(){
-        return this.email;
-    }
+//    public String getEmail(){
+//        return this.email;
+//    }
 
     public float mode(int num1, int num2) {
         result = num1 % num2;
